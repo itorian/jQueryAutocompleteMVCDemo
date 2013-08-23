4c72c67b24a38ccd7f97f2c3dc2c7f9f87cf54d0
itorian <abhikumarvatsa@yahoo.co.in>
2013-08-23 14:29:53 +0530

    first commit

---
+++ jQueryAutoCompleteMVC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using jQueryAutoCompleteMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryAutoCompleteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        NorthwindEntities _employees = new NorthwindEntities();
+
+        public ActionResult QuickSearch(string term)
+        {
+            var result = GetEmployees(term).Select(a => new { value = a.FirstName });
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        private List<Employee> GetEmployees(string searchString)
+        {
+            return _employees.Employees
+            .Where(a => a.FirstName.Contains(searchString))
+            .ToList();
+        }
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
